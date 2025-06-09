102dd4272bda07be58e09e6afdaa170169f18c6b
Fatemeh <fesmailif@gmail.com>
2025-06-10 00:42:42 +1200

    Modify

---
+++ 004-databases_and_schemas.sql
@@ -0,0 +1,48 @@
+SELECT * FROM TASTY_BYTES.RAW_POS.MENU;
+
+
+
+---> see table metadata
+SELECT * FROM TASTY_BYTES.INFORMATION_SCHEMA.TABLES;
+
+SELECT * FROM TASTY_BYTES.INFORMATION_SCHEMA.DATABASES;
+
+
+
+---> create a test database
+CREATE DATABASE test_database;
+
+SHOW DATABASES;
+
+---> drop the database
+DROP DATABASE test_database;
+
+---> undrop the database
+UNDROP DATABASE test_database;
+
+SHOW DATABASES;
+
+---> use a particular database
+USE DATABASE test_database;
+
+---> create a schema
+CREATE SCHEMA test_schema;
+
+SHOW SCHEMAS;
+
+---> see metadata about your database
+DESCRIBE DATABASE TEST_DATABASE;
+
+---> drop a schema
+DROP SCHEMA test_schema;
+
+SHOW SCHEMAS;
+
+---> undrop a schema
+UNDROP SCHEMA test_schema;
+
+SHOW SCHEMAS;
+
+USE SCHEMA test_schema;
+
+SHOW SCHEMAS;
